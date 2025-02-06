a44b8658e9c90cfccd3153f35cd4f6e4b4560669
Maarten van Kessel <m.l.vankessel@erasmusmc.nl>
2025-02-06 14:20:05 +0000

    version 3.0.0

--- inst/doc/DefiningCohorts.R
+++ inst/doc/DefiningCohorts.R
@@ -2,25 +2,25 @@
 knitr::opts_chunk$set(echo = TRUE)
 
 ## ----Capr, eval=FALSE---------------------------------------------------------
-# library(Capr)
-# 
-# viralSinusitis <- cs(
-#   descendants(40481087),
-#   name = "viralSinusitis",
-#   id = "Viral Sinusitis"
-# )
-# 
-# viralSinusitisCohort <- cohort(
-#   entry = entry(
-#     conditionOccurrence(viralSinusitis),
-#     primaryCriteriaLimit = "First"
-#   ),
-#   exit = exit(
-#     endStrategy = observationExit()
-#   )
-# )
-# 
-# cohortSet <- list(ViralSinusitis = viralSinusitisCohort)
+#  library(Capr)
+#  
+#  viralSinusitis <- cs(
+#    descendants(40481087),
+#    name = "viralSinusitis",
+#    id = "Viral Sinusitis"
+#  )
+#  
+#  viralSinusitisCohort <- cohort(
+#    entry = entry(
+#      conditionOccurrence(viralSinusitis),
+#      primaryCriteriaLimit = "First"
+#    ),
+#    exit = exit(
+#      endStrategy = observationExit()
+#    )
+#  )
+#  
+#  cohortSet <- list(ViralSinusitis = viralSinusitisCohort)
 
 ## -----------------------------------------------------------------------------
 list.files(
